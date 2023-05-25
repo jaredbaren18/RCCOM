74a97955bd794f724888e6520e13e839caafc1e0
jonasquebrar <115613366+jonasquebrar@users.noreply.github.com>
2023-05-25 17:21:35 +0800

    s

--- portal/resources/views/user/layout/layout.blade.php
+++ portal/resources/views/user/layout/layout.blade.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>ORMIN RED CROSS</title>
-    {{-- <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{asset('admin/plugins/fontawesome-free/css/all.min.css')}}">
     <link rel="stylesheet" href="{{asset('user/css/user.css')}}">
-     --}}
+    
 
   <script src="https://cdn.tailwindcss.com"></script>
   <script src="//unpkg.com/alpinejs" defer></script>
